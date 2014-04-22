a0a7b031f978273c87cafe2e0feac16dd708eaa5
mmichaud93 <mmichaud93@gmail.com>
2014-04-22 10:55:26 -0400

    admin revert

--- adminController.js
+++ adminController.js
@@ -78,10 +78,10 @@ function saveInsults() {
 	console.log(data);
 	var xhr = new XMLHttpRequest();
 	xhr.open('POST','http://calm-wave-1864.herokuapp.com/save-insults',true);
-	xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
+	xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
 
   	// send the collected data as JSON
-  	xhr.send("insults="+JSON.stringify(data));
+  	xhr.send(JSON.stringify(data));
 }
 
 function newInsult() {
