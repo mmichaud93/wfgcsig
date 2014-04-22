a9a29533911e43465683fa39484fbbe0b7e1c240
mmichaud93 <mmichaud93@gmail.com>
2014-04-22 12:14:31 -0400

    try again

--- app.js
+++ app.js
@@ -39,8 +39,9 @@ app.get('/insults', function(request, response) {
 });
 app.post('/save-insults', function(request, response) {
 	var outputFilename = './insults/insults.json';
-    var data = JSON.stringify(request);
-    console.log("request.query = "+data);
+    var data = response.body;
+    console.log("data = "+data);
+    console.log("data.insults = "+data.insults);
     fs.writeFile(outputFilename, data, function(err) {
         if(err) {
             console.log(err);
